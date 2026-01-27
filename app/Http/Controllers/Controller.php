1d79aac9c408004c6ac4c6fbe5033a5b5a723be0
milos.glogovac <milos.glogovac@gmail.com>
2026-01-27 18:34:09 +0100

    swagger ip address

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,7 +12,7 @@ use OpenApi\Attributes as OA;
     OA\Info(version: "1.0.0", description: "Messenger backend", title: "Messenger backend api"),
     OA\Schema(format:"https"),
     OA\Server(url: 'http://crypt-talk.test/api', description: "Dev server"),
-    OA\Server(url: 'http://165.232.113.33/api', description: "Dev server"),
+    OA\Server(url: 'http://95.217.156.195/api', description: "Dev server"),
     OA\SecurityScheme( securityScheme: 'bearerAuth', type: "http", name: "Authorization", in: "header", scheme:"Bearer", description: "Authorize with bearer token"),
     OA\Contact(email: "tcom.developer@gmail.com")
 ]
