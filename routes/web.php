7f2d33575edced33b0769f014120fcf5776d6f88
milos.glogovac <milos.glogovac@gmail.com>
2026-02-02 14:20:37 +0100

    landing page

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,25 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
-    // xdebug_info();
-phpinfo();
-    return 'pocetna';
+    return view('landing-page');
 });
 
-Route::get('/test-mail', function () {
-    try {
-        \Mail::raw('Test email', function ($message) {
-            $message->to('test@example.com')
-                    ->subject('Test Email');
-        });   
-    } catch (\Exception $e) {
-        return response()->json([
-            'error' => $e->getMessage(),
-            'trace' => $e->getTraceAsString()
-        ]);
-    }
-});
+// Route::get('/', function () {
+//     // xdebug_info();
+// phpinfo();
+//     return 'pocetna';
+// });
+
+// Route::get('/test-mail', function () {
+//     try {
+//         \Mail::raw('Test email', function ($message) {
+//             $message->to('test@example.com')
+//                     ->subject('Test Email');
+//         });   
+//     } catch (\Exception $e) {
+//         return response()->json([
+//             'error' => $e->getMessage(),
+//             'trace' => $e->getTraceAsString()
+//         ]);
+//     }
+// });
